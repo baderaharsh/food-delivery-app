34b2ed1ebf674440f0cb64f6726d970df72d6658
Harsh <baderaharsh@gmail.com>
2022-08-01 20:19:07 +0400

    Done. v1.0

--- src/components/Layouts/HeaderCartButton.js
+++ src/components/Layouts/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext,useState,useEffect } from 'react'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 import CartContext from '../../store/cart-context'
@@ -6,13 +6,34 @@ import CartContext from '../../store/cart-context'
 
 const HeaderCartButton = props => {
 
+    const [animate, setAnimate] = useState(false);
     const cartCtx = useContext(CartContext);
-
-    const cartItemsTotal = cartCtx.items.reduce((current, item) => {
+    
+    const { items } = cartCtx;
+    
+    const cartItemsTotal = items.reduce((current, item) => {
         return current + item.amount
     }, 0);
 
-    return <button className={classes.button} onClick={props.onClick}>
+    const buttonClasses = `${classes.button} ${animate ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        
+        setAnimate(true);
+        const timer = setTimeout(() => {
+            setAnimate(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+
+    }, [items])
+
+    return <button className={buttonClasses} onClick={props.onClick}>
         <span className={classes.icon}><CartIcon /></span>
         <span>Cart</span>
         <span className={classes.badge}>{cartItemsTotal}</span>
